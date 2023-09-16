5ac42a7aaa1ae457470a34b1f3796eccb7c3365f
firhan <firhan@jmdigital.id>
2023-09-16 08:39:31 +0700

    feat(client): add vitest testing library

---
+++ client/src/atoms/TodoDate/TodoDate.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+import TodoDate from './TodoDate'
+import '@testing-library/jest-dom'
+
+describe("TodoDate", () => {
+	it("should render date when it given", () => {
+		render(<TodoDate date='2 minutes ago'/>)
+
+		const dateElement = screen.getByRole("todo-date")
+
+		expect(dateElement).toHaveTextContent("2 minutes ago")
+	})
+})
--- client/src/atoms/TodoDate/TodoDate.tsx
+++ client/src/atoms/TodoDate/TodoDate.tsx
@@ -2,6 +2,6 @@ import { Text } from "@radix-ui/themes";
 
 export default function TodoDate({date}: {date: string}){
 	return (
-		<Text as="div">{ date }</Text>
+		<Text as="div" role="todo-date">{ date }</Text>
 	)
 }
